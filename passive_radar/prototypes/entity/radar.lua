c4fc43e5e0ff3b2ff2f231d779a5ade802e90df9
Ryan Delaney <ryan.patrick.delaney+git@protonmail.com>
2022-11-21 19:32:24 -0500

    rotation_speed = 0

--- passive_radar/prototypes/entity/radar.lua
+++ passive_radar/prototypes/entity/radar.lua
@@ -7,5 +7,6 @@ radar_passive.name = "radar-passive"
 radar_passive.minable.result = "radar"
 radar_passive.max_distance_of_sector_revealed = radar_range
 radar_passive.max_distance_of_nearby_sector_revealed = radar_range
+radar_passive.rotation_speed = 0
 
 data:extend({radar_passive})
